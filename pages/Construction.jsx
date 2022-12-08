952f753376b56efed9298ceff59d44d9942966f1
Khamdanu Syakir <64400505+anoderb@users.noreply.github.com>
2022-12-09 04:39:44 +0700

    Update Construction.jsx

--- pages/Construction.jsx
+++ pages/Construction.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import ConstructionImg from '../public/assets/Construction.png';
+import ConstructionImg from '../assets/Construction.png';
 import Footer from '../components/Footer'
 
 const Construction = () => {
