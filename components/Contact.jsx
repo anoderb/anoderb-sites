240d05172b91312adad13c0f5b8fd0bccb63de53
Khamdanu Syakir <64400505+anoderb@users.noreply.github.com>
2022-12-09 04:44:31 +0700

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -5,7 +5,7 @@ import { AiOutlineMail } from 'react-icons/ai';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
 import { FaGithub, FaTwitter, FaInstagram, FaDiscord, FaTelegram, FaYoutube } from 'react-icons/fa';
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
-import ContactImg from '../assets/contact.jpg';
+import ContactImg from '../assets/Contact.jpg';
 
 const Contact = () => {
   return (
